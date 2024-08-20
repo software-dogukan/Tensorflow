133d8a22efb6fdacd628825bc07a073e47a44b97
Dogukan Alkan <78702122+software-dogukan@users.noreply.github.com>
2024-08-21 00:47:39 +0300

    create model

---
+++ Tensorflow_Create_Model.py
@@ -0,0 +1,5 @@
+import keras
+inputs = keras.Input(shape=(37,))
+x = keras.layers.Dense(32, activation="relu")(inputs)
+outputs = keras.layers.Dense(5, activation="softmax")(x)
+model = keras.Model(inputs=inputs, outputs=outputs)
